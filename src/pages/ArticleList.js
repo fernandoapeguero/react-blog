50103a21f0de3e6d8aa029f93b996f771ef0877c
fernandoapeguero <fernandoapeguero@gmail.com>
2022-05-31 09:24:21 -0400

    Add list of article to articles page

--- src/pages/ArticleList.js
+++ src/pages/ArticleList.js
@@ -1,10 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import articlecontent from "./article-content";
 
 const ArticleList = () => {
   return (
-    <div>
+    <>
       <h1>Articles</h1>
-    </div>
+
+      {articlecontent.map((article, key) => (
+        <Link
+          className="article-list-item"
+          key={key}
+          to={`/article/${article.name}`}
+        >
+          <h3>{article.title}</h3>
+          <p>{article.content.toString().substring(0, 150)}</p>
+        </Link>
+      ))}
+    </>
   );
 };
 
