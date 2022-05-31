aff1313e773950181ea8b23917d19c99fe481666
fernandoapeguero <fernandoapeguero@gmail.com>
2022-05-31 10:36:47 -0400

    Add NotFoundPage functionality to ArticlePage

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -2,13 +2,14 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import ArticlesList from "../components/ArticlesList";
 import articles from "./article-content";
+import NotFoundPage from "./NotFoundPage";
 
 const ArticlePage = () => {
   const { name } = useParams();
 
   const currentArticle = articles.find((article) => article.name === name);
 
-  if (!currentArticle) return <h1>Article does not exists.</h1>;
+  if (!currentArticle) return <NotFoundPage />;
 
   const otherArticles = articles.filter(
     (article) => article.name !== currentArticle.name
